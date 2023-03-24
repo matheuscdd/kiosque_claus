00a65afc6d40881c586b1eaa192f59e52d98bc31
Claus <matheuscdd@hotmail.com>
2023-03-24 13:29:45 -0300

    feat: tarefa Add Product  finalizada

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-from management.product_handler import get_product_by_id, get_products_by_type
+
 
 if __name__ == "__main__":
-    print(get_products_by_type("drink"))
     ...
--- management/product_handler.py
+++ management/product_handler.py
@@ -1,12 +1,19 @@
 from menu import products
 
 
-def get_product_by_id(identifier: int):
+def get_product_by_id(identifier: int) -> dict:
     result = list(filter(lambda product: product["_id"] == identifier, products))
     return result[0] if len(result) else dict()
 
 
-def get_products_by_type(kind: str):
+def get_products_by_type(kind: str) -> list:
     result = list(filter(lambda product: product["type"] == kind, products))
     return result if len(result) else list()
+
+
+def add_product(menu: list[dict], **kwargs) -> dict:
+    curr_id = menu[len(menu)-1]["_id"] + 1 if len(menu) else 1
+    kwargs["_id"] = curr_id
+    menu.append(kwargs)
+    return kwargs
 
